407143bcc4e91a3d7a157fb847a906416f16cbaf
dynamodenis <dmbugua66@gmail.com>
2020-06-05 12:08:51 +0300

    project methods created and all method test passed

--- project/tests.py
+++ project/tests.py
@@ -1,6 +1,30 @@
 from django.test import TestCase
 from .models import Project
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestProject(TestCase):
+    def setUp(self):
+        self.user=User(username='denis')
+        self.user.save()
+        self.project=Project(user=self.user, title='test', description='this is a test project')
+        self.project.save()
+    
+    #TEST METHOD    
+    def test_save(self):
+        self.project.save_project()
+        project=Project.objects.all()
+        self.assertTrue(len(project)==1)
+     
+    #TEST DELETE METHOD   
+    def test_delete(self):
+        Project.delete_project(self.project.pk)
+        after_delete=Project.objects.all()
+        self.assertTrue(len(after_delete)==0)
+        
+    #TEST GET ELEMENT BY D
+    def test_get_by_id(self):
+        project=Project.get_project(self.project.id)
+        self.assertEqual(project.title,'test')
+        
     
