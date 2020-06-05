5ba4f22853177fc4fb9ff6acd6f8c073859161a3
dynamodenis <dmbugua66@gmail.com>
2020-06-05 18:57:19 +0300

    created a media and static urls

--- awwards_project/settings.py
+++ awwards_project/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -137,6 +138,8 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL='project:project_index'
 LOGIN_URL='login'
