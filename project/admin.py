32ed45f945e026dd78ad0c32e88bc3ccfc678d3d
dynamodenis <dmbugua66@gmail.com>
2020-06-05 12:18:48 +0300

    added the models admin module

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project
 
+admin.site.site_header='AwwardMe Admin'
+admin.site.site_title='AwwardMe Admin Dashboard'
 # Register your models here.
+admin.site.register(Project)
